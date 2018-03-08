e3bd1e360c4cd25f86554bd7b14e9714a8eaef94
Unknown <edu_sumiya@globo.com>
2018-03-07 23:45:24 -0300

    Initial Commit

---
+++ Teste DCX/Teste DCXTests/Teste_DCXTests.swift
@@ -0,0 +1,36 @@
+//
+//  Teste_DCXTests.swift
+//  Teste DCXTests
+//
+//  Created by Eduardo Sumiya on 07/03/18.
+//  Copyright © 2018 Eduardo Sumiya. All rights reserved.
+//
+
+import XCTest
+@testable import Teste_DCX
+
+class Teste_DCXTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
